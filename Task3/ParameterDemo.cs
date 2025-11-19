43cb6202a575ab9f4baa5d9d68317dab98cc7119
Bipulz <bhandaribipul345@gmail.com>
2025-11-19 21:34:59 +0545

    Task-3 completed

---
+++ Task3/ParameterDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace task3
+{
+    internal class ParameterDemo
+    {
+        public int Increase(ref int number)
+        {
+            // adds 10 to the number using ref
+            number += 10;
+            return number;
+        }
+
+        public void GetFullName(out string fullName)
+        {
+            // assigning a default full name
+            fullName = "Karan Thapa";
+        }
+
+        public int SumAll(params int[] numbers)
+        {
+            // sums all numbers passed in params
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Task 3!");
+
+            ParameterDemo demo = new ParameterDemo();
+
+            int value = 5;
+            Console.WriteLine($"Original value: {value}");
+            int increasedValue = demo.Increase(ref value);
+            Console.WriteLine($"Value after Increase(): {increasedValue}");
+
+            demo.GetFullName(out string fullName);
+            Console.WriteLine("Full Name: " + fullName);
+
+            int sum = demo.SumAll(1, 2, 3, 4, 5);
+            Console.WriteLine("Sum of all numbers: " + sum);
+        }
+    }
+}
