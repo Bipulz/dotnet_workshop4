11d25a779ba450e2aff6e920ab3868ee56fbee99
Bipulz <bhandaribipul345@gmail.com>
2025-11-19 22:15:16 +0545

    Task-5 completed

---
+++ Task5/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    // record to store book details
+    record Book(string title, string author, double price);
+}
