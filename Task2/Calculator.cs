a65a591fa85b6f368b1188dc0813f7a48772a016
Bipulz <bhandaribipul345@gmail.com>
2025-11-19 12:13:38 +0545

    Task2 completed

---
+++ Task2/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Calculator
+    {
+        public void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the Simple Calculator!");
+        }
+
+        public int Add(int num1, int num2)
+        {
+            // returns the sum of two numbers
+            return num1 + num2;
+        }
+
+        public int Multiply(int num1, int num2 = 5)
+        {
+            // multiplies with default value if second number is not provided
+            return num1 * num2;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Task 2!");
+
+            Calculator calculator = new Calculator();
+
+            calculator.PrintWelcome();
+
+            int sum = calculator.Add(10, 20);
+            Console.WriteLine($"Sum Result: {sum}");
+
+            int product = calculator.Multiply(10);
+            Console.WriteLine($"Product Result: {product}");
+        }
+    }
+}
